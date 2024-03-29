9084551d33f983ac2cdd4c3b99f98c5d3d21fd4c
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-03-28 18:21:20 -0700

    15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,7 +13,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	if ((tree->left != NULL && tree->right == NULL) ||
-		(tree->left == NULL && tree->right != NULL) ||)
+			(tree->left == NULL && tree->right != NULL))
 		return (0);
 
 	return (binary_tree_is_full(tree->left) &&
